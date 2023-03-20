100fdbeb8698865b6f9648d45d54844b16899541
aimlinux <114636143+aimlinux@users.noreply.github.com>
2023-03-20 15:12:41 +0900

    commit

--- main_folder/script/main.js
+++ main_folder/script/main.js
@@ -1,9 +1,2 @@
-alert('おはようみあ');
+//alert('おはようみあ');
 //console.log('おはようみあ'); 
-
-const ham = $('#js-hamburger'); //js-hamburgerの要素を取得し、変数hamに格納
-const nav = $('#js-nav'); //js-navの要素を取得し、変数navに格納
-ham.on('click', function () { //ハンバーガーメニューをクリックしたら
-  alert("ok!"); //コンソール画面でokというメッセージが出る
-
-});
