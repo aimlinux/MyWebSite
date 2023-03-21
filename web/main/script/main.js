7af713df9fec416f6836f0c74232a2760e71d991
aimlinux <114636143+aimlinux@users.noreply.github.com>
2023-03-21 21:30:19 +0900

    commit

--- web/main/script/main.js
+++ web/main/script/main.js
@@ -1,2 +1,13 @@
 //alert('おはよう');
 //console.log('おはよう'); 
+
+
+
+//お問い合わせフォームについて
+$( '.js-input' ).keyup(function() {
+    if( $(this).val() ) {
+        $(this).addClass('not-empty');
+    } else {
+        $(this).removeClass('not-empty');
+    }
+});
