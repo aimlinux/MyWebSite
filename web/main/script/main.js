7151a294accbb1045f5688d64b888ab2d02050e2
aimlinux <114636143+aimlinux@users.noreply.github.com>
2023-04-07 11:35:52 +0900

    scroll-behavior: smooth;

--- web/main/script/main.js
+++ web/main/script/main.js
@@ -2,24 +2,27 @@
 //console.log('おはよう'); 
 
 
+
 //ページトップボタン...
 jQuery(function() {
     var appear = false;
     var pagetop = $('#page-top');
     pagetop.hide();
     $(window).scroll(function () {
-        if ($(this).scrollTop() > 200) {  //100pxスクロールしたら表示
+        if ($(this).scrollTop() > 100) {  //100pxスクロールしたら表示
             pagetop.fadeIn();
         } else {
             pagetop.fadeOut();
         }
     });
-
+/*
     pagetop.click(function () {
         $('body, html').animate({ scrollTop: 0 }, 700); //0.5秒かけてトップへ戻る
         return false;
     });
+*/
 });
+
 
 
 
